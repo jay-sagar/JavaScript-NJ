561a6e16ef3c743cee43842ad5fa205dac8b0026
Jay Sagar <jaysagar871@gmail.com>
2025-01-05 22:37:01 +0530

    throttling

---
+++ 21_Throttling.js
@@ -0,0 +1,25 @@
+// Throttling => It is a technique in which, no matter how many times the user fires the event, the attached function will be executed only once in a given time interval.
+// It will help to reduce the number of times the function is called.
+
+let counter = 0;
+const expensive = () => {
+    console.log('expensive function.....', counter++);
+}
+
+window.addEventListener('resize', betterFunction());
+
+const betterFunction = throttle(expensive, 300);
+
+function throttle(fn, limit) {
+    let flag = true;
+    return function () {
+        let context = this, args = arguments;
+        if (flag) {
+            fn.apply(context, args);
+            flag = false;
+            setTimeout(() => {
+                flag = true;
+            }, limit);
+        }
+    }
+}
